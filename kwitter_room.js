7bc02ae5112ec7d5ff5119008f13e7ded1df3874
Bhishmakvij <79918644+Bhishmakvij@users.noreply.github.com>
2021-12-12 15:35:12 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDj1SwWayb2enO0BC9P-85afBZ82ZQj80c",
+  authDomain: "project-let-chat-web-app.firebaseapp.com",
+  projectId: "project-let-chat-web-app",
+  storageBucket: "project-let-chat-web-app.appspot.com",
+  messagingSenderId: "148236990778",
+  appId: "1:148236990778:web:27c5037ec36f9aac1e7803"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
